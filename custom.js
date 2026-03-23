34323859eda4fbc4089d0e8cc246be2cce072b43
Mazenabadla <mznabadla@gmain.com>
2026-03-23 17:52:03 +0200

    modification

--- custom.js
+++ custom.js
@@ -1,1 +1,5 @@
+<<<<<<< HEAD
 document.getElementByID("BOSS").INNERHTML="hello"
+=======
+document.getElementById('pge').innerHTML = "how are you"
+>>>>>>> d884992 (content modified in both index.html and custom.js)
